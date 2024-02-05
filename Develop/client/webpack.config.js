7e3907e86002a8b0f8c95fb7b5e520150504d870
TavonnS <142205891+TavonnS@users.noreply.github.com>
2024-02-05 10:25:14 -0800

    sw

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -14,6 +14,8 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
+      database: './src/js/database.js',
+      index: './src/js/index.js',
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
@@ -23,7 +25,7 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html', // path
         filename: 'index.html',
-        chunks: ['main'],
+        // chunks: ['main'],
       }),
       new WebpackPwaManifest({
         name: 'Writer',
@@ -31,20 +33,19 @@ module.exports = () => {
         description: 'A simple note taking app',
         background_color: '#ffffff',
         theme_color: '#000000',
-        start_url: '/client/index.html', 
-        filename: '/client/manifest.json',
+        start_url: '/', 
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve('./src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
-            
           },
         ],
-      }),
+ }),
       new InjectManifest({
         swSrc: './src-sw.js', // Path to your service worker file
-        swDest: 'service-worker.js', // Output service worker file
+        swDest: './service-worker.js', // Output service worker file
       }),
     ],
     module: {
